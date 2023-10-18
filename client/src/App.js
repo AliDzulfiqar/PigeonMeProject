4cdb275b6d0c81e10e953160591b02d71814b8da
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-10-17 21:47:50 -0700

    Create options on main menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import io from "socket.io-client"
+import { useState } from "react"
 
 const socket = io.connect("http://localhost:3001")
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (username !== "" && room !== ""){
+
+    }
+  }
+
   return (
     <div className="App">
-      
+      <h3>Join Chat</h3>
+      <input type="text" placeholder="Name" onChange={(event) => setUsername(event.target.value)}/>
+      <input type="text" placeholder="Room ID" onChange={(event) => setRoom(event.target.value)}/>
+      <button>Join Room</button>
     </div>
   );
 }
