bfe60a386ff333e7b07c4707fe43e76ffd358ca0
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-10-17 22:08:04 -0700

    Created UI

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Chat = (socket, username, room) => {
+    const [currentMessage, SetCurrentMessage] = useState("");
+    const sendMessage = async () =>{
+        if (currentMessage !== ""){
+            const messageData = {
+                room: room,
+                sender: username,
+                message: currentMessage,
+                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+            }
+        }
+    }
+
     return (
         <div>
             <div className="chat__header">
@@ -8,7 +20,9 @@ const Chat = (socket, username, room) => {
             </div>
             <div className="chat__body"></div>
             <div className="chat__footer">
-                <input type="text" placeholder='Send your message...'/>
+                <input type="text" placeholder='Send your message...' onChange={(event) => {
+                    SetCurrentMessage(event.target.value);
+                }}/>
                 <button>Send</button>    
             </div> 
         </div>
