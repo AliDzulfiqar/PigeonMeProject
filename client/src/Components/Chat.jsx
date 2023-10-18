ccf92e1bdec3f87fb36cc805676bc679b4f9f4ac
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-10-17 22:02:31 -0700

    Added basic UI for Chat Component

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Chat = (socket, username, room) => {
     return (
-        <div> 
+        <div>
+            <div className="chat__header">
+                <p>Chat Now!</p>
+            </div>
+            <div className="chat__body"></div>
+            <div className="chat__footer">
+                <input type="text" placeholder='Send your message...'/>
+                <button>Send</button>    
+            </div> 
         </div>
     );
 };
