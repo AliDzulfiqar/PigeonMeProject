3cb33cb9a5b299625af25047c0c3797b63d92905
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-10-18 21:03:21 -0700

    Changed divname

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="app">
       <section id="create-room">
         <h1 className="create-room--header">Welcome to PigeonMe!</h1>
         <div className="form-wrapper">
